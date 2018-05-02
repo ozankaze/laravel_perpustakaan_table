0f91f2bc763d91c6da83a6576ed173a7d4c5db38
ozankaze <ozankaze101@gmail.com>
2018-05-02 09:23:03 +0700

    Penggunaan Resfull Resource

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function() {
-
+    Route::resource('authors', 'AuthorsController');
 });
