405a9db5383d7c26555637d2e06baaad3de58660
ozankaze <ozankaze101@gmail.com>
2018-05-05 13:28:48 +0700

    .

--- app/Author.php
+++ app/Author.php
@@ -18,25 +18,25 @@ class Author extends Model
 
     public static function boot()
     {
-    parent::boot();
+        parent::boot();
 
-    self::deleting(function($author) {
-    // mengecek apakah penulis masih punya buku
-        if ($author->books->count() > 0) {
-        // menyiapkan pesan error
-        $html = 'Penulis tidak bisa dihapus karena masih memiliki buku : ';
-        $html .= '<ul>';
-        foreach ($author->books as $book) {
-        $html .= "<li>$book->title</li>";
-        }
-        $html .= '</ul>';
-            Session::flash("flash_notification", [
-            "level"=>"danger",
-            "message"=>$html
-        ]);
-        // membatalkan proses penghapusan
-        return false;
-        }
-    });
+        self::deleting(function($author) {
+        // mengecek apakah penulis masih punya buku
+            if ($author->books->count() > 0) {
+            // menyiapkan pesan error
+            $html = 'Penulis tidak bisa dihapus karena masih memiliki buku : ';
+            $html .= '<ul>';
+            foreach ($author->books as $book) {
+            $html .= "<li>$book->title</li>";
+            }
+            $html .= '</ul>';
+                Session::flash("flash_notification", [
+                "level"=>"danger",
+                "message"=>$html
+            ]);
+            // membatalkan proses penghapusan
+            return false;
+            }
+        });
     }
 }
