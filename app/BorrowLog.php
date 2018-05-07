64866e2ed75bbbbce7a22b840ae2a26a065de2c1
ozankaze <ozankaze101@gmail.com>
2018-05-07 14:25:02 +0700

    Penggunaan Attribute Casting pada Eloquent

--- app/BorrowLog.php
+++ app/BorrowLog.php
@@ -10,6 +10,13 @@ class BorrowLog extends Model
         'book_id', 'user_id', 'is_returned'
     ];
 
+    protected $casts = [
+        'is_returned' => 'boolean',
+        /* terdapat fitur attribute casting yang akan otomatis merubah nilai
+        sebuah field ketika diambil atau disimpan,
+        is_returned menjadi nilai true atau false . */
+    ];
+
     public function book()
     {
         return $this->belongsTo(Book::class);
